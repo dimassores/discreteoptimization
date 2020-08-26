ac26d2777a93ce71af98184df949e6ed9f656916
Dimas Soares <dimassoareslima@gmail.com>
2020-08-26 10:12:46 -0300

    hello world

---
+++ Week_1/anyint/solver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def solve_it(input_data):
+    # return a positive integer, as a string
+    return '86'
+
+
+if __name__ == '__main__':
+    solve_it('')
