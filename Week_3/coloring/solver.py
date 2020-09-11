31a0edea44958bc0345f352c46ec739cd58073e6
Dimas Soares <dimassoareslima@gmail.com>
2020-09-11 11:53:08 -0300

    solver update

--- Week_3/coloring/solver.py
+++ Week_3/coloring/solver.py
@@ -1,43 +1,90 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import numpy as np
+from pulp import *
 
 
-def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
-
-    first_line = lines[0].split()
-    node_count = int(first_line[0])
-    edge_count = int(first_line[1])
-
-    edges = []
-    for i in range(1, edge_count + 1):
+def structure(input_data: str):
+    """
+    """
+    lines = input_data.split("\n")
+    firstLine = lines[0].split()
+    number_of_vertices = int(firstLine[0])
+    number_of_edges = int(firstLine[1])
+    items_conections = np.around(np.empty([number_of_vertices, number_of_vertices]))
+    for i in range(1, number_of_edges + 1):
         line = lines[i]
         parts = line.split()
-        edges.append((int(parts[0]), int(parts[1])))
+        items_conections[int(parts[0]), int(parts[1])] = 1
+        items_conections[int(parts[1]), int(parts[0])] = 1
 
-    # build a trivial solution
-    # every node has its own color
-    solution = range(0, node_count)
+    return items_conections, number_of_edges, number_of_vertices
 
-    # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, solution))
 
+def read_data(file_location: str):
+    with open(file_location, "r") as input_data_file:
+        output_data = input_data_file.read()
     return output_data
 
 
-import sys
+def solve_it(input_data: str):
+    items_conections, number_of_edges, number_of_vertices = structure(input_data)
+    number_of_possible_colors = number_of_vertices * 0.2  # magic number :/
+    y = range(number_of_possible_colors)
+    nodes = range(number_of_vertices)
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        file_location = sys.argv[1].strip()
-        with open(file_location, 'r') as input_data_file:
-            input_data = input_data_file.read()
-        print(solve_it(input_data))
-    else:
-        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
+    # initializes lp problem
+    lp = LpProblem("Coloring Problem", LpMinimize)
 
+    # variables x_ij to indicate whether node i is colored by color j;
+    xij = LpVariable.dicts("x", (nodes, y), cat="Binary")
+
+    # variables yj to indicate whether color j was used
+    yj = LpVariable.dicts("y", y, cat="Binary")
+
+    # objective is the sum of yj over all j
+    obj = lpSum(yj[j] for j in y)
+    lp += obj, "Objective Function"
+
+    # constraint s.t. each node uses exactly 1 color
+    for r in nodes:
+        jsum = 0.0
+        for j in y:
+            jsum += xij[r][j]
+        lp += jsum == 1, ""
+
+    # constraint s.t. adjacent nodes do not have the same color
+    for row in range(0, number_of_vertices):
+        for col in range(0, number_of_vertices):
+            if items_conections[row, col] == 1:
+                for j in y:
+                    lp += xij[row][j] + xij[col][j] <= 1, ""
+
+    # constraint s.t. if node i is assigned color k, color k is used
+    for i in nodes:
+        for j in y:
+            lp += xij[i][j] <= yj[j], ""
+
+    # constrinat for upper bound on # of colors used
+    lp += lpSum(yj[j] for j in y) <= number_of_possible_colors, ""
+
+    # solves lp and prints optimal solution/objective value
+    lp.solve()
+
+    all_variables = [
+        variable.name for variable in lp.variables() if variable.varValue != 0
+    ]
+
+    # setting the structure to be as the assigment asks
+    colors_index = [item.split("_")[1] for item in all_variables if "y_" in item]
+    color_of_each_item = [item.split("_")[2] for item in all_variables if "x_" in item]
+    opt_value = len(colors_index)
+    output_data = str(int(opt_value)) + " " + str(0) + "\n"
+    output_data += " ".join(map(str, color_of_each_item))
+    return output_data
+
+
+if __name__ == "__main__":
+    input_data = read_data(sys.argv[1])
+    solve_it(input_data)
